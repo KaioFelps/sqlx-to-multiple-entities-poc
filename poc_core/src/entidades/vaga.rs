6509a7fb7753796b1d1c7f32a8441b6499f49569
KaioFelps <kaiofelipe2005@gmail.com>
2025-07-13 04:01:08 -0300

    change(Vaga entity): manually implement `FromRow`

--- poc_core/src/entidades/vaga.rs
+++ poc_core/src/entidades/vaga.rs
@@ -1,17 +1,30 @@
-use sqlx::prelude::FromRow;
+use sqlx::{Row, postgres::PgRow, prelude::FromRow};
 use uuid::Uuid;
 
 use crate::agregados::projeto_com_coordenadores::ProjetoComCoordenadores;
 
-#[derive(Debug, FromRow)]
+#[derive(Debug)]
 pub struct Vaga {
     pub id: Uuid,
     pub titulo_customizado: Option<String>,
-    #[sqlx(flatten)]
     pub projeto: ProjetoComCoordenadores,
     pub detalhes: String,
-    #[sqlx(try_from = "i16")]
     pub quantidade: u8,
-    #[sqlx(try_from = "i16")]
     pub horas_p_semana: u8,
+}
+
+impl<'r> FromRow<'r, PgRow> for Vaga {
+    fn from_row(row: &'r PgRow) -> Result<Self, sqlx::Error> {
+        let qtd: i16 = row.try_get("quantidade")?;
+        let hrs: i16 = row.try_get("horas_p_semana")?;
+
+        Ok(Self {
+            id: row.try_get("id")?,
+            titulo_customizado: row.try_get("titulo_customizado")?,
+            detalhes: row.try_get("detalhes")?,
+            quantidade: qtd as u8,
+            horas_p_semana: hrs as u8,
+            projeto: ProjetoComCoordenadores::from_row(row)?,
+        })
+    }
 }
